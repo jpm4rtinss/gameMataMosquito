d0187740779e52f3affa8c0ff6a7787367482e7b
João Pedro Martins <joaopedromsn@gmail.com>
2020-08-16 01:36:55 -0300

    alternate size and side

--- game.js
+++ game.js
@@ -7,21 +7,49 @@ function ajustSizeGameplace(){
 }
 ajustSizeGameplace()
 
-//generate random positions
-var positionY = Math.floor(Math.random()*gameplaceH) - 90
-var positionX = Math.floor(Math.random()*gameplaceW) - 90
-positionY = positionY < 0 ? 0 : positionY
-positionX = positionX < 0 ? 0 : positionX
+function randomElement(){
+     //generate random positions
+     var positionY = Math.floor(Math.random()*gameplaceH) - 90
+     var positionX = Math.floor(Math.random()*gameplaceW) - 90
+     positionY = positionY < 0 ? 0 : positionY
+     positionX = positionX < 0 ? 0 : positionX
  
-// create element html
-var mosquito = document.createElement('img')
-mosquito.src = 'img/mosca.png'
+     // create element html
+     var mosquito = document.createElement('img')
+     mosquito.src = 'img/mosca.png'
 
-// set style and position 
-mosquito.className = 'mosquito1'
-mosquito.style.left= positionX + 'px'
-mosquito.style.top= positionY + 'px'
-mosquito.style.position= 'absolute'
+     // set style and position 
+     mosquito.className = mosquitoClass() + ' ' +reverseMosquito()
+     mosquito.style.left= positionX + 'px'
+     mosquito.style.top= positionY + 'px'
+     mosquito.style.position= 'absolute'
 
-//append element 
-document.body.appendChild(mosquito)
+     //append element 
+     document.body.appendChild(mosquito)
+}
+randomElement()
+//alternate style classes
+function mosquitoClass (){
+    var numberMosquitoClass = Math.floor(Math.random()*3)
+    
+    switch(numberMosquitoClass){
+        case 0:
+            return 'mosquito0'
+        case 1:
+            return 'mosquito1'
+        case 2:
+            return 'mosquito2'
+     }
+}
+
+// reverse mosquito img
+function reverseMosquito() {
+    var changeSide = Math.floor(Math.random()*2)
+    
+    switch(changeSide){
+        case 0:
+            return 'sideA'
+        case 1:
+            return 'sideB'
+     }
+}
